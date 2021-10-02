7c49b8ef0683d90de713a85e72f96bfb0b2efe02
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2021-10-02 15:35:21 +0530

    adding join and sendMessage using socket io

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInroom } = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
-const { callbackify } = require('util');
 
 const app = express();
 const server = http.createServer(app);
@@ -23,9 +24,25 @@ app.use(cors());
 app.use(router);
 
 io.on('connection', (socket) =>{
-    console.log('we have a connection!!!');
-    socket.on('join', ({name, room}) => {
-        console.log(name, room)
+    socket.on('join', ({name, room}, callback) => {
+      const { error, user } = addUser({ id: socket.id, name, room })
+
+      if(error) return callback(error)
+
+      socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+      socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`})
+
+      socket.join(user.room)
+
+      callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+      const user = getUser(socket.id)
+
+      io.to(user.room).emit('message', { user: user.name, text: message })
+
+      callback()
     })
 
     socket.on('disconnect', () => {
